867a8db2114c2da361ad700b672719e6da757e11
leonardgee <gw980531@gmail.com>
2021-12-12 16:12:43 +0800

    Removed unused constants.

--- src/utils/pubkeys.ts
+++ src/utils/pubkeys.ts
@@ -24,23 +24,3 @@ export const SOL_MINT = new PublicKey(
 export const WRAPPED_SOL_MINT = new PublicKey(
   "So11111111111111111111111111111111111111112"
 );
-
-export const WORM_MARKET_BASE = new PublicKey(
-  "6a9wpsZpZGxGhFVSQBpcTNjNjytdbSA1iUw1A5KNDxPw"
-);
-
-export const WORM_USDC_MINT = new PublicKey(
-  "FVsXUnbhifqJ4LiXQEbpUtXVdB8T5ADLKqSs5t1oc54F"
-);
-
-export const WORM_USDC_MARKET = new PublicKey(
-  "6nGMps9VfDjkKEwYjdSNqN1ToXkLae4VsN49fzBiDFBd"
-);
-
-export const WORM_USDT_MINT = new PublicKey(
-  "9w97GdWUYYaamGwdKMKZgGzPduZJkiFizq4rz5CPXRv2"
-);
-
-export const WORM_USDT_MARKET = new PublicKey(
-  "4v6e6vNXAaEunrvbqkYnKwbaWfck8a2KVR4uRAVXxVwC"
-);
